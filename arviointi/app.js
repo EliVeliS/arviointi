1f50bdd1706cdc405ef63a1f451d51e36f318330
EliVeliS <t0seel00@students.oamk.fi>
2021-10-14 22:27:22 +0300

    arviointisivu luotu

--- arviointi/app.js
+++ arviointi/app.js
@@ -38,6 +38,7 @@ app.use(flash());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/opiskelija', opiskelijaRouter);
 app.use('/arviointi', arviointiRouter);
 
 // catch 404 and forward to error handler
