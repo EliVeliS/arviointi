9724204f1d8533880cba88d72b6b9506c05d080d
EliVeliS <t0seel00@students.oamk.fi>
2021-10-14 21:29:20 +0300

    serveriä muokattu

--- arviointi/routes/opiskelija.js
+++ arviointi/routes/opiskelija.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 var dbConn  = require('../lib/db');
  
-// display books page
+// näytä opiskelijasivu
 router.get('/', function(req, res, next) {
       
-    dbConn.query('SELECT * FROM opiskelija ORDER BY idOpiskelija',function(err,rows)     {
+    dbConn.query('select * from opiskelija order by idOpiskelija',function(err,rows)     {
  
         if(err) {
             req.flash('error', err);
@@ -21,31 +21,32 @@ router.get('/add', function(req, res, next) {
     res.render('opiskelija/add', {
         Etunimi: '',
         Sukunimi: '',
-	Osoite: '',
-	Luokkatunnus: ''        
+        Osoite: '',
+        Luokkatunnus: ''        
     })
 })
 
-//lisätään opiskelija
+// lisätään opiskelija
 router.post('/add', function(req, res, next) {    
 
     let Etunimi = req.body.Etunimi;
     let Sukunimi = req.body.Sukunimi;
     let Osoite = req.body.Osoite;
     let Luokkatunnus = req.body.Luokkatunnus;
-    let errors = false;
+    let  errors = false;
 
-    if(Etunimi.length === 0 || Sukunimi.length === 0 || Osoite.length === 0 || Luokkatunnus.lenght === 0) {
+    if(Etunimi.length === 0 || Sukunimi.length === 0|| Osoite.length === 0|| Luokkatunnus.length === 0) {
         errors = true;
 
         // set flash message
-        req.flash('virhe', "Kirjoita kaikki tarvittavat tiedot oikein");
+        req.flash('virhe', "Anna kaikki tarvittavat tiedot");
         // render to add.ejs with flash message
         res.render('opiskelija/add', {
+            idOpiskelija: req.params.idOpiskelija,
             Etunimi: Etunimi,
             Sukunimi: Sukunimi,
-	    Osoite: Osoite,
-	    Luokkatunnus: Luokkatunnus
+            Osoite: Osoite,
+            Luokkatunnus: Luokkatunnus
         })
     }
 
@@ -55,8 +56,8 @@ router.post('/add', function(req, res, next) {
         var form_data = {
             Etunimi: Etunimi,
             Sukunimi: Sukunimi,
-	    Osoite: Osoite,
-	    Luokkatunnus: Luokkatunnus
+            Osoite: Osoite,
+            Luokkatunnus: Luokkatunnus
         }
         
         // insert query
@@ -69,12 +70,12 @@ router.post('/add', function(req, res, next) {
                 res.render('opiskelija/add', {
                     Etunimi: form_data.Etunimi,
                     Sukunimi: form_data.Sukunimi,
-		    Osoite: form_data.Osoite,
-		    Luokkatunnus: form_data.Luokkatunnus                      
+	    Osoite: form_data.Osoite,
+	    Luokkatunnus: form_data.Luokkatunnus                    
                 })
             } else {                
                 req.flash('Onnistui!', 'Opiskelijan tiedot lisätty kantaan.');
-                res.redirect('/Opiskelija');
+                res.redirect('/opiskelija');
             }
         })
     }
@@ -83,17 +84,17 @@ router.post('/add', function(req, res, next) {
 // muokkaa opiskelija taulua
 router.get('/edit/(:idOpiskelija)', function(req, res, next) {
 
-    let idOpiskelija = req.params.idOpiskelija;
+    let idOpiskelija = req.params.id;
    
     dbConn.query('SELECT * FROM opiskelija WHERE idOpiskelija = ' + idOpiskelija, function(err, rows, fields) {
         if(err) throw err
          
         // jos opiskelijaa ei löydy
         if (rows.length <= 0) {
-            req.flash('Virhe', 'Opiskelijaa ei löytynyt id:llä = ' + idOpiskelija)
+            req.flash('error', 'Opiskelijaa ei loytynyt id:lla = ' + idOpiskelija)
             res.redirect('/opiskelija')
         }
-        // jos opiskelija löytyi
+        // jos oiskelija löytyi
         else {
             // render to edit.ejs
             res.render('opiskelija/edit', {
@@ -101,8 +102,8 @@ router.get('/edit/(:idOpiskelija)', function(req, res, next) {
                 idOpiskelija: rows[0].idOpiskelija,
                 Etunimi: rows[0].Etunimi,
                 Sukunimi: rows[0].Sukunimi,
-	        Osoite: rows[0].Osoite,
-		Luokkatunnus: rows[0].Luokkatunnus
+	Osoite: rows[0].Osoite,
+	Luokkatunnus: rows[0].Luokkatunnus,
             })
         }
     })
@@ -115,21 +116,20 @@ router.post('/update/:idOpiskelija', function(req, res, next) {
     let Etunimi = req.body.Etunimi;
     let Sukunimi = req.body.Sukunimi;
     let Osoite = req.body.Osoite;
-    let Luokkatunnus = req.body.Luokkatunnus;	
+    let Luokkatunnus = req.body.Luokkatunnus;
     let errors = false;
 
-    if(Etunimi.length === 0 || Sukunimi.length === 0 || Osoite.length === 0 || Luokkatunnus.lenght === 0) {
+if(Etunimi.length === 0 || Sukunimi.length === 0|| Osoite.length === 0|| Luokkatunnus.length === 0) {
         errors = true;
         
         // set flash message
-        req.flash('Virhe', "Kirjoita kaikki tarvittavat tiedot oikein");
+        req.flash('Virhe', "Anna kaikki tarvittavat tiedot");
         // render to add.ejs with flash message
-        res.render('opiskelija/edit', {
-            idOpiskelija: req.params.idOpsikelija,            
+        res.render('opiskelija/add', {
             Etunimi: Etunimi,
             Sukunimi: Sukunimi,
-	    Osoite: Osoite,
-	    Luokkatunnus: Luokkatunnus
+            Osoite: Osoite,
+            Luokkatunnus: Luokkatunnus
         })
     }
 
@@ -139,25 +139,25 @@ router.post('/update/:idOpiskelija', function(req, res, next) {
         var form_data = {
             Etunimi: Etunimi,
             Sukunimi: Sukunimi,
-	    Osoite: Osoite,
-	    Luokkatunnus: Luokkatunnus
+            Osoite: Osoite,
+            Luokkatunnus: Luokkatunnus
         }
         // update query
-        dbConn.query('UPDATE opiskelija SET ? WHERE idOpsikelija = ' + idOpiskelija, form_data, function(err, result) {
+        dbConn.query('UPDATE opiskelija SET ? WHERE idOpiskelija = ' + idOpiskelija, form_data, function(err, result) {
             //if(err) throw err
             if (err) {
                 // set flash message
-                req.flash('Virhe', err)
+                req.flash('virhe', err)
                 // render to edit.ejs
-                res.render('opiskelija/edit', {
-                id: req.params.idOpsikelija,            
-                Etunimi: Etunimi,
-                Sukunimi: Sukunimi,
-	        Osoite: Osoite,
-	        Luokkatunnus: Luokkatunnus
+            res.render('opiskelija/add', {
+            idOpiskelija: req.params.idOpiskelija,
+            Etunimi: Etunimi,
+            Sukunimi: Sukunimi,
+            Osoite: Osoite,
+            Luokkatunnus: Luokkatunnus
                 })
             } else {
-                req.flash('Onnistui!', 'Opiskelijan tiedot lisätty kantaan');
+                req.flash('Onnistui!', 'Opiskelijan tiedot lisatty kantaan');
                 res.redirect('/opiskelija');
             }
         })
@@ -178,7 +178,7 @@ router.get('/delete/(:idOpiskelija)', function(req, res, next) {
             res.redirect('/opiskelija')
         } else {
             // set flash message
-            req.flash('Onnistui!', 'Opiskelijan tiedot poistettu kannasta ID = ' + idOpiskelija)
+            req.flash('Onnistui!', 'Opiskelijan tiedot poistettu ID = ' + idOpiskelija)
             res.redirect('/opiskelija')
         }
     })
